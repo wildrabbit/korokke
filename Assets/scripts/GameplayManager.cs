aec8779227aa1c7318eb5b5e813c266ee62d8bfa
Wild Rabbit <ithiliel@gmail.com>
2017-08-25 23:56:19 +0200

    Added lost condition (d'oh)

--- Assets/scripts/GameplayManager.cs
+++ Assets/scripts/GameplayManager.cs
@@ -103,7 +103,7 @@ public class GameplayManager : MonoBehaviour
         }
         else
         {
-            StartCoroutine(GameWon());
+            StartCoroutine(GameLost());
         }
         StopGame();
 
